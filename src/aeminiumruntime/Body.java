be94f8659d2c40293f6fa69f588268ffcbcc1f14
Sven Stork <svens@cs.cmu.edu>
2010-06-20 22:48:48 +0000

    - use more sensible name

--- src/aeminiumruntime/Body.java
+++ src/aeminiumruntime/Body.java
@@ -2,5 +2,5 @@ package aeminiumruntime;
 
 /* represents the task body that is executed by the runtime */
 public interface Body {
-    public void execute(Task parent);
+    public void execute(Task current);
 }
