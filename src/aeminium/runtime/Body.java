4b9db6672f8924fe5949c132029d963f1deb785d
Sven Stork <svens@cs.cmu.edu>
2010-10-20 13:13:11 +0000

    - allow body to throw exception

--- src/aeminium/runtime/Body.java
+++ src/aeminium/runtime/Body.java
@@ -9,5 +9,5 @@ public interface Body {
 	 * 
 	 * @param current Reference to the associated task object.
 	 */
-    public void execute(Runtime rt, Task current);
+    public void execute(Runtime rt, Task current) throws Exception;
 }
