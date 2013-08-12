102d05fc41fc08f4813d4c7b991776b57a3c8ff2
Alcides Fonseca <me@alcidesfonseca.com>
2013-08-12 22:15:16 +0100

    Lazy Binary Splitting implemented.

--- src/aeminium/runtime/helpers/loops/ForTask.java
+++ src/aeminium/runtime/helpers/loops/ForTask.java
@@ -38,14 +38,43 @@ public class ForTask {
 
 				while (bottom < top) {
 					boolean shouldCheck = (bottom % PPS == 0);
-					if (shouldCheck && rt.parallelize()) {
+					if (shouldCheck && (top-bottom > PPS) && rt.parallelize()) {
 						int half = (top - bottom)/2 + bottom;
 						Task otherHalf = rt.createNonBlockingTask(createLazyBinarySplitting(half, top, collection, forBody, hints), (short) (hints | Hints.LOOPS));
 						rt.schedule(otherHalf, current, Runtime.NO_DEPS);
 						top = half;
 					} else {
 						forBody.iterate(collection.get(bottom), rt, current);
-						bottom++;
+						bottom ++;
+					}
+				}
+
+			}
+		};
+	}
+
+	public static Task createFor(Runtime rt, final Range collection,
+			final ForBody<Integer> forBody, final short hints) {
+		return rt.createNonBlockingTask(createLazyBinarySplitting(collection.getStart(), collection.getEnd(), collection.getIncrement(), forBody, hints), Runtime.NO_HINTS);
+	}
+
+	public static Body createLazyBinarySplitting(final int start, final int end, final int increment, final ForBody<Integer> forBody, final short hints) {
+		return new Body() {
+			@Override
+			public void execute(Runtime rt, Task current) throws Exception {
+				int bottom = start;
+				int top = end;
+
+				while (bottom < top) {
+					boolean shouldCheck = (bottom % PPS == 0);
+					if (shouldCheck && (top-bottom > PPS) && rt.parallelize()) {
+						int half = (top - bottom)/2 + bottom;
+						Task otherHalf = rt.createNonBlockingTask(createLazyBinarySplitting(half, top, increment, forBody, hints), (short) (hints | Hints.LOOPS));
+						rt.schedule(otherHalf, current, Runtime.NO_DEPS);
+						top = half;
+					} else {
+						forBody.iterate(bottom, rt, current);
+						bottom += increment;
 					}
 				}
 
