e81a9c0d0a2ec8a8207fead39673f8e5799f6975
Alcides Fonseca <me@alcidesfonseca.com>
2013-08-13 11:49:40 +0100

    Added MaxLevel decider

---
+++ src/aeminium/runtime/implementations/implicitworkstealing/decider/MaxLevel.java
@@ -0,0 +1,22 @@
+package aeminium.runtime.implementations.implicitworkstealing.decider;
+
+import aeminium.runtime.Runtime;
+import aeminium.runtime.implementations.Configuration;
+import aeminium.runtime.implementations.implicitworkstealing.task.ImplicitTask;
+
+public class MaxLevel implements ParallelizationDecider {
+
+	protected final int maxLevel  = Configuration.getProperty(getClass(), "maxLevelThreshold", 16);
+
+	
+	@Override
+	public void setRuntime(Runtime rt) {
+		// No need
+	}
+
+	@Override
+	public boolean parallelize(ImplicitTask current) {
+		return current.level < maxLevel;
+	}
+
+}
