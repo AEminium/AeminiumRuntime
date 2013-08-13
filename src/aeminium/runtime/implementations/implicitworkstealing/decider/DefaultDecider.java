fc0093380bf39584ba66f628d7c1526295b883ea
Alcides Fonseca <me@alcidesfonseca.com>
2013-08-13 12:19:32 +0100

    Default Decider is LoadBased

--- src/aeminium/runtime/implementations/implicitworkstealing/decider/DefaultDecider.java
+++ src/aeminium/runtime/implementations/implicitworkstealing/decider/DefaultDecider.java
@@ -1,29 +1,6 @@
 package aeminium.runtime.implementations.implicitworkstealing.decider;
 
-import aeminium.runtime.Runtime;
-import aeminium.runtime.implementations.Configuration;
-import aeminium.runtime.implementations.implicitworkstealing.ImplicitWorkStealingRuntime;
-import aeminium.runtime.implementations.implicitworkstealing.scheduler.WorkStealingThread;
-import aeminium.runtime.implementations.implicitworkstealing.task.ImplicitTask;
 
-public class DefaultDecider implements ParallelizationDecider {
-
-	protected final int parallelizeThreshold  = Configuration.getProperty(getClass(), "parallelizeThreshold", 3);
-	protected ImplicitWorkStealingRuntime runtime = null;
-	
-	@Override
-	public boolean parallelize(ImplicitTask current) {
-		int c=0;
-		for (WorkStealingThread thread: runtime.scheduler.getThreads()) {
-			c += thread.getTaskQueue().size(); 
-		}
-		if (c < runtime.scheduler.getMaxParallelism() * parallelizeThreshold) return true;
-		return false;
-	}
-
-	@Override
-	public void setRuntime(Runtime rt) {
-		this.runtime = (ImplicitWorkStealingRuntime) rt;
-	}
-
+public class DefaultDecider extends LoadBased {
+	// That's it.
 }
