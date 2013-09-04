c6f044c52d73d1cb8d3bd4f460ff5186c5968373
Alcides Fonseca <me@alcidesfonseca.com>
2013-09-04 11:39:24 +0100

    Added MaxTasksWithStackSize.

---
+++ src/aeminium/runtime/implementations/implicitworkstealing/decider/MaxTasksWithStackSize.java
@@ -0,0 +1,33 @@
+package aeminium.runtime.implementations.implicitworkstealing.decider;
+
+import aeminium.runtime.Runtime;
+import aeminium.runtime.implementations.Configuration;
+import aeminium.runtime.implementations.implicitworkstealing.ImplicitWorkStealingRuntime;
+import aeminium.runtime.implementations.implicitworkstealing.scheduler.WorkStealingThread;
+import aeminium.runtime.implementations.implicitworkstealing.task.ImplicitTask;
+
+public class MaxTasksWithStackSize implements ParallelizationDecider {
+
+	ImplicitWorkStealingRuntime rt;
+
+	protected final int maxTotalTasksPerCoreThreshold = Configuration.getProperty(getClass(), "maxTotalTasksPerCoreThreshold", 2);
+	protected final int maxStackSize  = Configuration.getProperty(getClass(), "maxStackSize", 13);
+
+	@Override
+	public void setRuntime(Runtime rt) {
+		this.rt = (ImplicitWorkStealingRuntime) rt;
+	}
+
+	@Override
+	public boolean parallelize(ImplicitTask current) {
+		if (Thread.currentThread().getStackTrace().length > maxStackSize) return false;
+		
+		int totalTasks = rt.scheduler.getSubmissionQueueSize();
+		for (WorkStealingThread thread : rt.scheduler.getThreads()) {
+			totalTasks += thread.getTaskQueue().size();
+		}
+		return (totalTasks < rt.scheduler.getMaxParallelism()
+				* maxTotalTasksPerCoreThreshold);
+	}
+
+}
