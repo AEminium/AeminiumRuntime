b8ad45ea61990750ce05be89858e66b9dc1d4a69
Alcides Fonseca <me@alcidesfonseca.com>
2025-10-09 15:00:55 +0100

    Alterações pedidas pelo professor

--- src/aeminium/runtime/examples/futures/fib/FutureFib.java
+++ src/aeminium/runtime/examples/futures/fib/FutureFib.java
@@ -7,15 +7,15 @@ public class FutureFib {
 	public static void main(String[] args) {
 		RuntimeManager.init();
 		FutureFib f = new FutureFib();
-		System.out.println(f.fib(5));
+		System.out.println(f.fib(15));
 		RuntimeManager.shutdown();
 	}
 	
 	public int fib(int n) {
 		if (n<2) return 1;
 		else {
-			Future<Integer> a1 = new Future<Integer>((t) -> fib(n-1));
-			Future<Integer> a2 = new Future<Integer>((t) -> fib(n-2));
+			Future<Integer> a1 = new Future<>((t) -> fib(n-1));
+			Future<Integer> a2 = new Future<>((t) -> fib(n-2));
 			return a1.get() + a2.get();
 		}
 	}
